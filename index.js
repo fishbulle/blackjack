f7336b66bb92227f142f325a110026be8969a740
Angelina <linaemalmros@gmail.com>
2022-07-21 13:28:28 +0200

    changed button style

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ let message = " "
 
 function startGame() {
     if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    console.log(message)
 }
-}
-
-console.log(message)
