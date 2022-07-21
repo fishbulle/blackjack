06a2c5fa64e3c04589f9d8cc1465d7cd6aeb05cd
Angelina <linaemalmros@gmail.com>
2022-07-21 12:43:25 +0200

    code additions

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let firstCard = 11
 let secondCard = 11
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = " "
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
-} 
-else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")
+    message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else {
+    message = "You're out of the game!"
+    isAlive = false
 }
-else {
-    console.log("You're out of the game!")
-}
+
+console.log(message)
