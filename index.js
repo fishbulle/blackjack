59a71e7469083a03f48754e6755fbd5cffe0561b
Angelina <linaemalmros@gmail.com>
2022-07-21 12:16:52 +0200

    changed if condition

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let firstCard = 11
 let secondCard = 11
 let sum = firstCard + secondCard
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card?")
 } 
 else if (sum === 21) {
